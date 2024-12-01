6935b2d75f431893bf65c14a6843f91272bc6e2b
om00sman <omoosman@ucsc.edu>
2024-11-30 16:38:20 -0800

    Update samtools_filter.sh

--- samtools_filter.sh
+++ samtools_filter.sh
@@ -23,3 +23,14 @@ module load samtools
 samtools view -bh /hb/home/omoosman/owen/zoarcoidei/analysis/realignment/a_lupus/a_lupus_alignment.sorted.bam h1tg000009l h1tg000038l h1tg000066l h2tg000001l h2tg000051l h2tg000193l -o /hb/home/omoosman/owen/zoarcoidei/analysis/realignment/a_lupus/a_lupus_filt_AFP.bam
 
 samtools index /hb/home/omoosman/owen/zoarcoidei/analysis/realignment/a_lupus/a_lupus_filt_AFP.bam
+
+exonerate --model protein2genome \ 
+  --query /path/to/Mamericanus_AFP.txt \
+  --target /path/to/pgunn/hap_2_genome.fa \
+  --showtargetgff TRUE \
+  --showquerygff FALSE \
+  --minintron 0 \
+  --maxintron 10000 \
+  --showalignment FALSE --showcigar FALSE \
+  --ryo "Query: %qi Length: %ql Strand: %qs Target: %ti Range: %tcb-%tce\n" \
+  > /path/to/output/pgunn_hap2_AFP_annotations.gff
