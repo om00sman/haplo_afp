39ac1a58ad4f1fa1e4d0d6fd6e691e63eebff014
om00sman <omoosman@ucsc.edu>
2024-12-02 09:01:55 -0800

    Update filter_2.sh

--- filter_2.sh
+++ filter_2.sh
@@ -18,6 +18,14 @@
 
 module load samtools
 
+# defining variables
+
+config_file=/hb/groups/kelley_training/owen/zoarcoidei/data/sra_id.txt
+name=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$config_file" | awk '{print $2}')
+out=/hb/home/omoosman/owen/zoarcoidei/analysis/realignment/$name
+
+
+
 #filter with samtools and index output
 
 samtools view -bh /hb/home/omoosman/owen/zoarcoidei/analysis/realignment/a_lupus/a_lupus_alignment.sorted.bam h1tg000009l h1tg000038l h1tg000066l h2tg000001l h2tg000051l h2tg000193l -o /hb/home/omoosman/owen/zoarcoidei/analysis/realignment/a_lupus/a_lupus_filt_AFP.bam
