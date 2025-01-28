d5fc3427db639fcbd697303088b722906faf7ae0
om00sman <omoosman@ucsc.edu>
2025-01-28 14:33:47 -0800

    Update hap_align.sh

--- hap_align.sh
+++ hap_align.sh
@@ -1,30 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=hap_align
+#SBATCH --job-name=mafft
 #SBATCH --mail-user=omoosman@ucsc.edu
 #SBATCH --mail-type=ALL
-#SBATCH --output=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/switch_err/hap_align_%A_%a.out
-#SBATCH --error=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/switch_err/hap_align_%A_%a.err
+#SBATCH --output=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/switch_err/mafft_%A_%a.out
+#SBATCH --error=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/switch_err/mafft_%A_%a.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=12
-#SBATCH --mem=70GB
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=20GB
 #SBATCH --time=5-00:00:00
 
 #load required modules
-module load minimap2
-module load samtools
+module load mafft
 
-#define variables
-config_file=/hb/groups/kelley_training/owen/zoarcoidei/data/sra_id.txt
-name=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$config_file" | awk '{print $2}')
-in=/hb/home/omoosman/owen/zoarcoidei/data/assemblies/$name
-out=/hb/home/omoosman/owen/zoarcoidei/analysis/realignment/$name
+"/hb/software/apps/mafft/gnu-7.520/mafftdir/bin/mafft"  --auto --inputorder "l_dearborni_contigs.fasta" > "l_dearborni_contig_aligned.fasta"
 
-#perform alignment
-
-minimap2 -t 12 -ax asm5 "$in/${name}_hap1_ctg.fasta" "$in/${name}_hap2_ctg.fasta" > "$out/${name}_haptohap.sam"
-
-#convert output to BAM and sort and index
-samtools view -b "$out/${name}_haptohap.sam" > "$out/${name}_haptohap.bam"
-samtools sort "$out/${name}_haptohap.bam" -o "$out/${name}_haptohap.sorted.bam"
-samtools index "$out/${name}_haptohap.sorted.bam"
