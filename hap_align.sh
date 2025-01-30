4de3a29e22e4b861fda975f9f27f5143c10af668
om00sman <omoosman@ucsc.edu>
2025-01-30 11:45:25 -0800

    Update hap_align.sh

--- hap_align.sh
+++ hap_align.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=mafft
+#SBATCH --job-name=kalign
 #SBATCH --mail-user=omoosman@ucsc.edu
 #SBATCH --mail-type=ALL
-#SBATCH --output=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/switch_err/mafft_%A_%a.out
-#SBATCH --error=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/switch_err/mafft_%A_%a.err
+#SBATCH --output=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/switch_err/kalign_%A_%a.out
+#SBATCH --error=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/switch_err/kalign_%A_%a.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
@@ -11,7 +11,8 @@
 #SBATCH --time=5-00:00:00
 
 #load required modules
-module load mafft
+module load miniconda3
+conda activate kalign
 
-"/hb/software/apps/mafft/gnu-7.520/mafftdir/bin/mafft"  --auto --inputorder "l_dearborni_contigs.fasta" > "l_dearborni_contig_aligned.fasta"
+kalign -i /hb/groups/kelley_training/owen/zoarcoidei/data/assemblies/l_dearborni/l_dearborni_contigs.fasta --type dna -o /hb/groups/kelley_training/owen/zoarcoidei/analysis/realignment/l_dearborni/l_dearborni_alignment.fasta
 
