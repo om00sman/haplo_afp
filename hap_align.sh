5474970ba4baee86773a8dce2c70ace03633dc5d
om00sman <omoosman@ucsc.edu>
2025-02-05 15:01:03 -0800

    Update hap_align.sh

--- hap_align.sh
+++ hap_align.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=kalign
+#SBATCH --job-name=hap_align
 #SBATCH --mail-user=omoosman@ucsc.edu
 #SBATCH --mail-type=ALL
-#SBATCH --output=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/switch_err/kalign_%A_%a.out
-#SBATCH --error=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/switch_err/kalign_%A_%a.err
+#SBATCH --output=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/switch_err/align_%A_%a.out
+#SBATCH --error=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/switch_err/align_%A_%a.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=20GB
+#SBATCH --mem=30GB
 #SBATCH --time=5-00:00:00
 
 #load required modules
 module load miniconda3
-conda activate kalign3
+conda activate mummer
 
-kalign -i /hb/groups/kelley_training/owen/zoarcoidei/data/assemblies/l_dearborni/l_dearborni_contigs.fasta --type dna -o /hb/groups/kelley_training/owen/zoarcoidei/analysis/realignment/l_dearborni/l_dearborni_alignment.fasta
+nucmer --threads=4 -p l_dearborni_h1tg000019l_h2tg000034l /hb/home/omoosman/owen/zoarcoidei/data/assemblies/l_dearborni/l_dearborni_h1tg000019l.fasta /hb/home/omoosman/owen/zoarcoidei/data/assemblies/l_dearborni/l_dearborni_h2tg000034l.fasta
 
