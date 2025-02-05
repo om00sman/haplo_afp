41b1d30936a61047f335a75b69a5af5299273556
om00sman <omoosman@ucsc.edu>
2025-02-05 15:10:40 -0800

    Update hap_align.sh

--- hap_align.sh
+++ hap_align.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=hap_align
 #SBATCH --mail-user=omoosman@ucsc.edu
 #SBATCH --mail-type=ALL
-#SBATCH --output=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/switch_err/align_%A_%a.out
-#SBATCH --error=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/switch_err/align_%A_%a.err
+#SBATCH --output=/hb/home/omoosman/owen/zoarcoidei/analysis/realignment/l_dearborni/mummer/align_%A_%a.out
+#SBATCH --error=/hb/home/omoosman/owen/zoarcoidei/analysis/realignment/l_dearborni/mummer/align_%A_%a.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
@@ -14,5 +14,5 @@
 module load miniconda3
 conda activate mummer
 
-nucmer --threads=4 -p --coords l_dearborni_h1tg000019l_h2tg000034l /hb/home/omoosman/owen/zoarcoidei/data/assemblies/l_dearborni/l_dearborni_h1tg000019l.fasta /hb/home/omoosman/owen/zoarcoidei/data/assemblies/l_dearborni/l_dearborni_h2tg000034l.fasta
+nucmer --threads=4 --coords -p l_dearborni_h1tg000019l_h2tg000034l /hb/home/omoosman/owen/zoarcoidei/data/assemblies/l_dearborni/l_dearborni_h1tg000019l.fasta /hb/home/omoosman/owen/zoarcoidei/data/assemblies/l_dearborni/l_dearborni_h2tg000034l.fasta
 
