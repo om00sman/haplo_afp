754ee014e8298d91a19d69d027dcebbf0687a55e
om00sman <omoosman@ucsc.edu>
2025-02-20 09:27:31 -0800

    Update dearborni_switch_err.sh

--- dearborni_switch_err.sh
+++ dearborni_switch_err.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bash
+#SBATCH --job-name=zoarcoidei_switch_err
+#SBATCH --mail-user=omoosman@ucsc.edu
+#SBATCH --mail-type=ALL
+#SBATCH --output=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/switch_err/switch_%A_%a.out
+#SBATCH --error=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/switch_err/switch_%A_%a.err
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=70GB
+#SBATCH --time=5-00:00:00
