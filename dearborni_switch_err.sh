ead85ea2d98f8c219f9ca70d5f38b4f9c28dc77a
om00sman <omoosman@ucsc.edu>
2025-02-20 09:30:17 -0800

    Update dearborni_switch_err.sh

--- dearborni_switch_err.sh
+++ dearborni_switch_err.sh
@@ -9,3 +9,16 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=70GB
 #SBATCH --time=5-00:00:00
+
+
+###### Realignment of haploptype specific reads to haplotype assemblies for dearborni, using different parameters #######
+
+# loading required modules + conda environment
+
+module load miniconda3
+conda activate pbmm2 
+
+# defining variables
+
+in=/hb/home/omoosman/owen/zoarcoidei/data/assemblies/l_dearborni
+out=/hb/home/omoosman/owen/zoarcoidei/analysis/realignment/l_dearborni
