e58ed13fbeed420cc8826fb7e87fc8dc0f2f6e74
om00sman <omoosman@ucsc.edu>
2025-02-20 10:43:00 -0800

    Update dearborni_switch_err.sh

--- dearborni_switch_err.sh
+++ dearborni_switch_err.sh
@@ -46,14 +46,14 @@ o_a=$(samtools view "$out/${name}_alignment.sorted.bam" | awk '{print $1}' | gre
 o_p=$(samtools view "$out/${name}_alignment.sorted.bam" | awk '{print $1}' | grep -F -f "$out/${name}_p.txt" | wc -l)
 
 #calculate proportions with decimal places
-proportion_o_m=$(echo "scale=4; $original_m / $m" | bc)
-proportion_o_a=$(echo "scale=4; $original_a / $a" | bc)
-proportion_o_p=$(echo "scale=4; $original_p / $p" | bc)
+proportion_o_m=$(echo "scale=4; $o_m / $m" | bc)
+proportion_o_a=$(echo "scale=4; $o_a / $a" | bc)
+proportion_o_p=$(echo "scale=4; $o_p / $p" | bc)
 
 #output in easily readable format
 echo "The proportion of hap1 reads that align to the original aligment is $proportion_o_p" >> "$out/proportion.txt"
 echo "The proportion of hap2 reads that align to the original aligment is $proportion_o_m" >> "$out/proportion.txt"
-echo "The proportion of hap2 reads that align to the original aligment is $proportion_o_m" >> "$out/proportion.txt"
+echo "The proportion of unphased reads that align to the original aligment is $proportion_o_a" >> "$out/proportion.txt"
 echo "" >> "$out/proportion.txt"
 
 ###alignment with ccs preset
@@ -74,7 +74,7 @@ samtools index "$out/${name}_ccs_m_a_alignment.filt.sorted.bam"
 
 samtools index "$out/${name}_ccs_p_a_alignment.filt.sorted.bam"
 
-##check proportion of reads that aligned in orginal alignment 
+##check proportion of reads that aligned in ccs alignment 
 
 #calculate the number of reads of each type in the .bam file
 ccs_m=$(samtools view "$out/${name}_ccs_alignment.sorted.bam" | awk '{print $1}' | grep -F -f "$out/${name}_m.txt" | wc -l)
@@ -82,14 +82,14 @@ ccs_a=$(samtools view "$out/${name}_ccs_alignment.sorted.bam" | awk '{print $1}'
 ccs_p=$(samtools view "$out/${name}_ccs_alignment.sorted.bam" | awk '{print $1}' | grep -F -f "$out/${name}_p.txt" | wc -l)
 
 #calculate proportions with decimal places
-proportion__ccs_m=$(echo "scale=4; $original_m / $m" | bc)
-proportion__ccs_a=$(echo "scale=4; $original_a / $a" | bc)
-proportion__ccs_p=$(echo "scale=4; $original_p / $p" | bc)
+proportion__ccs_m=$(echo "scale=4; $ccs_m / $m" | bc)
+proportion__ccs_a=$(echo "scale=4; $ccs_a / $a" | bc)
+proportion__ccs_p=$(echo "scale=4; $ccs_p / $p" | bc)
 
 #output in easily readable format
-echo "The proportion of hap1 reads that align to the original aligment is $proportion_p" >> "$out/proportion.txt"
-echo "The proportion of hap2 reads that align to the original aligment is $proportion_m" >> "$out/proportion.txt"
-echo "The proportion of hap2 reads that align to the original aligment is $proportion_m" >> "$out/proportion.txt"
+echo "The proportion of hap1 reads that align to the original aligment is $proportion__ccs_m" >> "$out/proportion.txt"
+echo "The proportion of hap2 reads that align to the original aligment is $proportion__ccs_m" >> "$out/proportion.txt"
+echo "The proportion of hap2 reads that align to the original aligment is $proportion__ccs_m" >> "$out/proportion.txt"
 echo "" >> "$out/proportion.txt"
 
 
