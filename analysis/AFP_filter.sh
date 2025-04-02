12fc80e80d8637777bc2f68dddd21ca221c55602
om00sman <omoosman@ucsc.edu>
2025-04-02 11:54:26 -0700

    Update AFP_filter.sh

--- analysis/AFP_filter.sh
+++ analysis/AFP_filter.sh
@@ -20,9 +20,15 @@ module load samtools
 module load miniconda3
 conda activate exonerate
 
+#define variables
+
+config_file=/hb/groups/kelley_training/owen/zoarcoidei/data/sra_id.txt
+name=z_americanus
+out=/hb/home/omoosman/owen/zoarcoidei/analysis/realignment/$name
+
 #filter with samtools and index output
 "$out/${name}_filt_AFP_p_a.bam"
 
-samtools view -bh /hb/home/omoosman/owen/zoarcoidei/analysis/realignment/z_americanus/z_americanus_alignment.sorted.bam h1tg000092l h1tg000766l h2tg000180l h2tg000104l -o /hb/home/omoosman/owen/zoarcoidei/analysis/realignment/z_americanus/z_americanus_filt_AFP.bam
+samtools view -bh /hb/home/omoosman/owen/zoarcoidei/analysis/realignment/z_americanus/z_americanus_alignment.sorted.bam h1tg000092l h1tg000766l h2tg000180l h2tg000104l -o "$out/${name}_filt_AFP_p_a.bam"
 
-samtools index /hb/home/omoosman/owen/zoarcoidei/analysis/realignment/z_americanus/z_americanus_filt_AFP.bam
+samtools index "$out/${name}_filt_AFP_p_a.bam"
