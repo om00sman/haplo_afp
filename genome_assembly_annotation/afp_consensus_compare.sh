8375369f87ef4d236b7f8131f969c85ca1010f76
om00sman <omoosman@ucsc.edu>
2025-06-17 11:19:51 -0700

    Update afp_consensus_compare.sh

--- genome_assembly_annotation/afp_consensus_compare.sh
+++ genome_assembly_annotation/afp_consensus_compare.sh
@@ -1,48 +1,39 @@
 #!/bin/bash
-#SBATCH --job-name=zoarcoidei_exonerate
+#SBATCH --job-name=consensus_query
 #SBATCH --mail-user=omoosman@ucsc.edu
 #SBATCH --mail-type=ALL
-#SBATCH --output=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/exonerate/query_%A_%a.out
-#SBATCH --error=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/exonerate/query_%A_%a.err
+#SBATCH --output=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/exonerate/consensus_query_%A_%a.out
+#SBATCH --error=/hb/home/omoosman/owen/zoarcoidei/analysis/err_out/exonerate/consensus_query_%A_%a.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=5GB
+#SBATCH --mem=2GB
 #SBATCH --time=0-1:00:00
 
 #load required modules
 
 module load miniconda3
+conda activate exonerate
 
 #define variables
 
-config_file=/hb/groups/kelley_training/owen/zoarcoidei/data/sra_id.txt
-name=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$config_file" | awk '{print $2}')
+config_file=/hb/groups/kelley_training/owen/zoarcoidei/data/species.txt
+name=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$config_file")
 in=/hb/home/omoosman/owen/zoarcoidei/data/assemblies/$name
-out=/hb/home/omoosman/owen/zoarcoidei/analysis/exonerate/$name
+out=/hb/home/omoosman/owen/zoarcoidei/analysis/exonerate/$name/consensus_compare
 
 # Create the output directory 
 
 mkdir -p "$out"
 
-#convert gfa to fasta
+#exonerate query for with Mamericanus_AFP
 
-conda activate gfatools
+exonerate --model protein2genome --query /hb/home/omoosman/owen/zoarcoidei/analysis/Mamericanus_AFP.txt --target "$in/${name}_hap1_ctg.fasta"  --showtargetgff TRUE --showquerygff FALSE > "$out/${name}_hap1_Mamer_afp.gff"
 
-gfatools gfa2fa "$in/${name}_hifi.asm.bp.hap1.p_ctg.gfa" > "$in/${name}_hap1_ctg.fasta" 
+exonerate --model protein2genome --query /hb/home/omoosman/owen/zoarcoidei/analysis/Mamericanus_AFP.txt --target "$in/${name}_hap2_ctg.fasta"  --showtargetgff TRUE --showquerygff FALSE > "$out/${name}_hap2_Mamer_afp.gff"
 
-gfatools gfa2fa "$in/${name}_hifi.asm.bp.hap2.p_ctg.gfa" > "$in/${name}_hap2_ctg.fasta"
+#exonerate query for consensus AFP
 
-gfatools gfa2fa "$in/${name}_hifi.asm.bp.r_utg.gfa" > "$in/${name}_r_utg.fasta"
+exonerate --model protein2genome --query "$out/${name}_consensus_afp.fasta" --target "$in/${name}_hap1_ctg.fasta"  --showtargetgff TRUE --showquerygff FALSE > "$out/${name}_hap1_consensus_afp.gff"
 
-conda deactivate
-
-#exonerate query for AFPs 
-
-conda activate exonerate
-
-exonerate --model protein2genome --query /hb/home/omoosman/owen/zoarcoidei/analysis/Mamericanus_AFP.txt --target "$in/${name}_hap1_ctg.fasta"  --showtargetgff TRUE --showquerygff FALSE > "$out/${name}_hap1_AFP_exonerate_output.txt"
-
-exonerate --model protein2genome --query /hb/home/omoosman/owen/zoarcoidei/analysis/Mamericanus_AFP.txt --target "$in/${name}_hap2_ctg.fasta"  --showtargetgff TRUE --showquerygff FALSE > "$out/${name}_hap2_AFP_exonerate_output.txt"
-
-exonerate --model protein2genome --query /hb/home/omoosman/owen/zoarcoidei/analysis/Mamericanus_AFP.txt --target "$in/${name}_r_utg.fasta"  --showtargetgff TRUE --showquerygff FALSE > "$out/${name}_r_utg_AFP_exonerate_output.txt"
+exonerate --model protein2genome --query "$out/${name}_consensus_afp.fasta" --target "$in/${name}_hap2_ctg.fasta"  --showtargetgff TRUE --showquerygff FALSE > "$out/${name}_hap2_consensus_afp.gff"
