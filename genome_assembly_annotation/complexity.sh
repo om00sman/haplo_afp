389f9d6187557c1d45647e9e9f404f9ef1ac7970
om00sman <omoosman@ucsc.edu>
2025-09-25 12:21:09 -0700

    Delete genome_assembly_annotation/complexity.sh

--- genome_assembly_annotation/complexity.sh
+++
@@ -1,6 +0,0 @@
-species=l_maculatus
-in=/hb/groups/kelley_training/owen/zoarcoidei/data/assemblies/$species
-out=/hb/home/omoosman/owen/zoarcoidei/analysis/complexity
-
-gfatools view -l $(cat a_minor_trans1.txt) $in/${species}_hifi.asm.bp.r_utg.gfa > a_minor_trans1.gfa
-sed 's/, /,/g' a_minor_array.txt > a_minor_trans1_.txt
